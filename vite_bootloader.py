841bb3f5bbd373c8853a7368f188840c2bff07cd
Damon Weiss <damonweiss@gmail.com>
2025-08-11 22:14:34 -0400

    Update vite_bootloader.py

--- vite_bootloader.py
+++ vite_bootloader.py
@@ -250,10 +250,8 @@ def ensure_frontend(root: Path) -> Path:
                     host: "127.0.0.1",
                     port: 5173,
                     proxy: {
-                      "/api": {
-                        target: "http://127.0.0.1:5000",
-                        changeOrigin: true
-                      }
+                      "/api":       { target: "http://127.0.0.1:5000", changeOrigin: true },
+                      "/socket.io": { target: "http://127.0.0.1:5000", ws: true, changeOrigin: true }
                     }
                   }
                 });
